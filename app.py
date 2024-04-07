abd8a57bf62f09d806e8fe94bfc03cf40f47e238
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-04-07 14:36:26 -0300

    refactor: format code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-
-from config import Config, get_environment_config
-
+'''This is the entrypoint of the API'''
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+from config import get_environment_config
 
 db = SQLAlchemy()
 
@@ -14,17 +14,21 @@ app.config.from_object(get_environment_config())
 # Inicializa la conexion a la BD
 db.init_app(app)
 
-@app.before_first_request
+@app.before_request
 def initialize_database():
     """ Crea las tablas de la BD """
+    app.before_request_funcs[None].remove(initialize_database)
     db.create_all()
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
+    ''' Disconnect the session '''
+    print(exception)
     db.session.remove()
 
 @app.route("/")
 def test():
+    ''' Chequeo de que la API esta levantada '''
     return "Test ok!"
 
-app.run(port=5001, debug=True, host="0.0.0.0")
+app.run(port=5000, debug=True, host="0.0.0.0")
--- config.py
+++ config.py
@@ -1,33 +1,58 @@
+"""Database configuration"""
+
 import os
 import click
 
+
 class Config(object):
-   ENV = os.environ["ENV"] if "ENV" in os.environ else "DEVELOPMENT"
-   CSRF_ENABLED = True
-   SECRET_KEY = "this_is_a_secret_key"
-   SQLALCHEMY_TRACK_MODIFICATIONS = False
+    """Database configuration class"""
+
+    ENV = os.environ["ENV"] if "ENV" in os.environ else "DEVELOPMENT"
+    CSRF_ENABLED = True
+    SECRET_KEY = "this_is_a_secret_key"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
-   DEBUG = True
-   SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://" + os.environ["DB_USERNAME"] + ":" \
-                             + os.environ["DB_PASSWORD"] + "@" \
-                             + os.environ["DB_HOST"] + ":" \
-                             + os.environ["DB_PORT"] + "/" \
-                             + os.environ["DB_DATABASE"]
-   click.echo(SQLALCHEMY_DATABASE_URI)
+    """Config for development environment"""
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://"
+        + os.environ["DB_USERNAME"]
+        + ":"
+        + os.environ["DB_PASSWORD"]
+        + "@"
+        + os.environ["DB_HOST"]
+        + ":"
+        + os.environ["DB_PORT"]
+        + "/"
+        + os.environ["DB_DATABASE"]
+    )
+    click.echo(SQLALCHEMY_DATABASE_URI)
+
 
 class TestingConfig(Config):
-   DEBUG = False
-   SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://" + os.environ["DB_USERNAME"] + ":" \
-                             + os.environ["DB_PASSWORD"] + "@" \
-                             + os.environ["DB_HOST"] + ":" \
-                             + os.environ["DB_PORT"] + "/" \
-                             + os.environ["DB_DATABASE"]
-   
-def get_environment_config():
-   print(Config.ENV)
-   if Config.ENV == "TESTING":
-       return "config.TestingConfig"
-   elif Config.ENV == "DEVELOPMENT":
-       return "config.DevelopmentConfig"
-   
+    """Config for testing environment"""
+
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://"
+        + os.environ["DB_USERNAME"]
+        + ":"
+        + os.environ["DB_PASSWORD"]
+        + "@"
+        + os.environ["DB_HOST"]
+        + ":"
+        + os.environ["DB_PORT"]
+        + "/"
+        + os.environ["DB_DATABASE"]
+    )
+
+
+def get_environment_config() -> str:
+    """Return the current environment"""
+    if Config.ENV == "TESTING":
+        return "config.TestingConfig"
+    elif Config.ENV == "DEVELOPMENT":
+        return "config.DevelopmentConfig"
